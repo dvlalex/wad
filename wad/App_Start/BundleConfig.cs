5ded511c88379c0c38c0d9c5a1a360f0537f1f6d
dvlalex <dvlalex@gmail.com>
2014-02-06 16:17:47 +0200

    frontpage done
    -updated jquery
    -updated modernizr
    -added bootstrap
    
    -frontpage created

--- wad/App_Start/BundleConfig.cs
+++ wad/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace wad
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-migrate-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
@@ -38,6 +39,13 @@ namespace wad
                         "~/Content/themes/base/jquery.ui.datepicker.css",
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
+
+            // bootstrap additions
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                        "~/Content/bootstrap.css",
+                        "~/Content/bootstrap-theme.css"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/bootstrap.js"));
         }
     }
 }
